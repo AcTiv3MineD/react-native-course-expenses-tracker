5bac437b3352d1473b7bcfb0b3b0d779b9eeaaa2
Cesar Mendez <acmined@gmail.com>
2025-01-14 13:53:45 -0400

    ui: style ExpensesSummary

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,6 +1,7 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
+import { GlobalStyles } from "../../constants/styles";
 
 const DUMMY_EXPENSES = [
     {
@@ -31,7 +32,7 @@ const DUMMY_EXPENSES = [
 
 function ExpensesOutput({expenses, expensesPeriod}) {
     return (
-        <View>
+        <View style={styles.container}>
             <ExpensesSummary periodName={expensesPeriod} expenses={DUMMY_EXPENSES} />
             <ExpensesList expenses={DUMMY_EXPENSES} />
         </View> 
@@ -39,3 +40,11 @@ function ExpensesOutput({expenses, expensesPeriod}) {
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700,
+    },
+})
--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,14 +1,35 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function ExpensesSummary({periodName, expenses}) {
     const expensesTotal = expenses.reduce((acc, expense) => acc + expense.amount, 0);
 
     return (
-        <View>
-            <Text>{periodName}</Text>
-            <Text>${expensesTotal.toFixed(2)}</Text>
+        <View style={styles.container}>
+            <Text style={styles.period}>{periodName}</Text>
+            <Text style={styles.total}>${expensesTotal.toFixed(2)}</Text>
         </View>
     )
 }
 
 export default ExpensesSummary;
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        backgroundColor: GlobalStyles.colors.primary50,
+        borderRadius: 6,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    period: {
+        fontSize: 16,
+        color: GlobalStyles.colors.primary400,
+    },
+    total: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: GlobalStyles.colors.primary500,
+    }
+});
